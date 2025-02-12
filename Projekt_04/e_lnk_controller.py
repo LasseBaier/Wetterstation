268a13f7d7fcdba3ffcfc4da5bb8f884b04f0342
LasseBaier <156214374+LasseBaier@users.noreply.github.com>
2025-02-12 16:21:46 +0100

    average

--- Projekt_04/e_lnk_controller.py
+++ Projekt_04/e_lnk_controller.py
@@ -2,6 +2,8 @@ from machine import Pin, I2C
 import time
 import framebuf
 import sys
+from normal_layout import normal_layout
+from warn_layout import warn_layout
 
 # 24 Hours in sec
 Max_time = 24*60*60
@@ -53,25 +55,98 @@ class e_lnk_controller:
         Values = [entry[Value] for entry in self.hour_buffer]
         return min(Values), max(Values)
     
-    def show_min_max(self,temperature=1, humidity=2, co2=3, pressure=4):
-        Min_Max_pressure = self.min_max_values(pressure)
-        Min_Max_temperature = self.min_max_values(temperature)
-        Min_Max_humidity = self.min_max_values(humidity)
-        Min_Max_co2 = self.min_max_values(co2)
+    def temperature_trend(self):
         
-        self.min_max_buffer.fill(white)
+        """ saves all values of the temperature in temperature """
+        temperature = [entry[1] for entry in self.hour_buffer]
+        # calculates the average
+        avg_temperature = sum(temperature) / len(temperature)
+        
+        
+        """ Defines a temperature trend """
+        current_temperature = temperature[-1]
+        if current_temperature > avg_temperature // 100:
+            return "rising"
+        elif current_pressure < avg_temperature // 100:
+            return "falling"
+        else:
+            return "steady"
+
+
+    def weather_prediction(self):
+        
+        """ splits up each measured value of hour_buffer in a different array """
+        humidity    = [entry[2] for entry in self.hour_buffer]
+        pressure    = [entry[4] for entry in self.hour_buffer]
+        
+        
+        """ calculates all averages """
+        avg_temperature = sum(temperature) / len(temperature)
+        avg_humidity    = sum(humidity) / len(humidity)
+        avg_pressure    = sum(pressure) / len(pressure)
+        
+        
+        """ defines a trend for measured value """
+        current_pressure = pressure[-1]
+        pressure_trend = "steady"
+        if current_pressure > avg_pressure // 100:
+            pressure_trend = "rising"
+        elif current_pressure < avg_pressure // 100:
+            pressure_trend = "falling"
+        
+        current_humidity = humidity[-1]
+        humidity_trend = "steady"
+        if current_humidity > avg_humidity // 100:
+            humidity_trend = "rising"
+        elif current_humidity < avg_humidity // 100:
+            humidity_trend = "falling"
+            
+        if pressure_trend == "rising" and humidity_trend == "falling":
+            return "sunny weather"
+        elsif pressure_trend == "falling" and humidity_trend == "rising":
+            return "rainy weather"
+        else:
+            return "steady"
+
+    
+    def ShowValues(self,Error, temperature=1, humidity=2, co2=3, pressure=4):
+        LivePressure = self.bme.raw_pressure()
+        LiveTemperature = self.bme.raw_temperature()
+        LiveHumidity = self.bme.raw_humidity()
+        LiveCo2 = self.Pasco2.read_value()
+        
+        self.Min_Max_pressure = self.min_max_values(pressure)
+        self.Min_Max_temperature = self.min_max_values(temperature)
+        self.Min_Max_humidity = self.min_max_values(humidity)
+        self.Min_Max_co2 = self.min_max_values(co2)
+        
+        # Chose if warn or the normal Overlay should be displayed
+        if Error = 1:
+            if
+            layout = framebuf.FrameBuffer(warn_layout,200,200, framebuf.MONO_HMSB)
+        else:
+            layout = framebuf.FrameBuffer(normal_layout,200,200, framebuf.MONO_HMSB)
+        #fb.blit überschreibt den gesamten buffer mit weiß und fügt den rest dazu welcher schwarz werden soll
+        fb.blit(layout,0,0)
+        #self.min_max_buffer.fill(white)
         # show Pressure
-        self.min_max_buffer.text(f"Min:{Min_Max_pressure[0]}", 30, 61, black)
-        self.min_max_buffer.text(f"Max:{Min_Max_pressure[1]}", 30, 74, black)
+        self.min_max_buffer.text(f"Min:{self.Min_Max_pressure[0]}", 30, 61, black)
+        self.min_max_buffer.text(f"Max:{self.Min_Max_pressure[1]}", 30, 74, black)
+        self.min_max_buffer.text(f"Max:{LivePressure}", 30, 87, black)
         # show Temperature
-        self.min_max_buffer.text(f"Min:{Min_Max_temperature[0]}", 130, 9, black)
-        self.min_max_buffer.text(f"Max:{Min_Max_temperature[1]}", 130, 22, black)
+        self.min_max_buffer.text(f"Min:{self.Min_Max_temperature[0]}", 130, 9, black)
+        self.min_max_buffer.text(f"Max:{self.Min_Max_temperature[1]}", 130, 22, black)
+        self.min_max_buffer.text(f"Max:{LiveTemperature}", 130, 35, black)
+
         # show humidity
-        self.min_max_buffer.text(f"Min:{Min_Max_humidity[0]}", 130, 61, black)
-        self.min_max_buffer.text(f"Max:{Min_Max_humidity[1]}", 130, 74, black)
+        self.min_max_buffer.text(f"Min:{self.Min_Max_humidity[0]}", 130, 61, black)
+        self.min_max_buffer.text(f"Max:{self.Min_Max_humidity[1]}", 130, 74, black)
+        self.min_max_buffer.text(f"Max:{LiveHumidity}", 130, 87, black)
+
         # show co2
-        self.min_max_buffer.text(f"Min:{Min_Max_co2[0]}", 30, 9, black)
-        self.min_max_buffer.text(f"Max:{Min_Max_co2[1]}", 30, 22, black)
+        self.min_max_buffer.text(f"Min:{self.Min_Max_co2[0]}", 30, 9, black)
+        self.min_max_buffer.text(f"Max:{self.Min_Max_co2[1]}", 30, 22, black)
+        self.min_max_buffer.text(f"Max:{LiveCo2}", 30, 35, black)
         
         self.Epd.display(self.min_max_buffer)
 
